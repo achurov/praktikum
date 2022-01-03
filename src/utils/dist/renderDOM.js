fc270fa92a42d6655dcad10a972cff36a0cfbc10
Andrew <achurov@gmail.com>
2022-01-04 00:30:01 +0300

    1

--- src/utils/dist/renderDOM.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function render(query, block) {
-    var root = document.querySelector(query);
-    root.append(block.getElement());
-    return root;
-}
-exports["default"] = render;
